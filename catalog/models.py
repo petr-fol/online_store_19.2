06b912a2d093ee573b0b6f82e2df1d15f2d1e6ab
petr-fol <144835428+petr-fol@users.noreply.github.com>
2024-01-02 20:58:39 +0600

    push

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
+NULLABLE = {'null': True, 'blank': True}
+
 
 class Contact(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
-    message = models.TextField()
+    name = models.CharField(max_length=255, verbose_name='имя')
+    email = models.EmailField(verbose_name='почта')
+    message = models.TextField(verbose_name='сообщение')
+
+    # message = models.TextField(verbose_name='сообщение', **NULLABLE)
+    # message = models.TextField(verbose_name='сообщение', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} {self.email} {self.message}"
+
+    class Meta:
+        verbose_name = 'контакт'
+        verbose_name_plural = 'контакты'
